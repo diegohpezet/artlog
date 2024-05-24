e6133252f677f96de9749849144a50919162df07
diegohpezet <diegohpezet@gmail.com>
2024-05-24 10:46:39 -0300

    set up ejs template

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
+const path = require('path')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.set('views', express.static(__dirname + '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('ejs', require('express-ejs-extend'));
 app.set('view engine', 'ejs');
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 // Set up routes
+app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
 app.use('/users', require('./routes/users'));
 app.use('/pictures', require('./routes/pictures'));
-app.get('/', (req, res) => {
-  res.render("index");
-});
 
 app.listen(port, () => {
   console.log(`MyApp listening on port ${port}`)
