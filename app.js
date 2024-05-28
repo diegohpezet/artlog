a9dabab894aa5a8bf80eaa4a971ec65747f1a912
diegohpezet <diegohpezet@gmail.com>
2024-05-28 13:45:23 -0300

    add bootstrap-icons

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.engine('ejs', require('express-ejs-extend'));
 app.set('view engine', 'ejs');
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/icons', express.static(__dirname + '/node_modules/bootstrap-icons/font'));
 
 // Set up routes
 app.use('/', require('./routes/index'));
