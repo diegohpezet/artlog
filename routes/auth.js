259a71ea412728599fc73680a3767db7c760df38
diegohpezet <diegohpezet@gmail.com>
2024-06-05 17:01:04 -0300

    feat: signout route

--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,6 @@ const AuthController = require('../controllers/AuthController');
 
 router.post('/signin', AuthController.signin);
 router.post('/signup', AuthController.signup);
+router.get('/signout', AuthController.signout);
 
 module.exports = router
