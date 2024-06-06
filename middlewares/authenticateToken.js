800f9e062cbf4449ef2969405e28f8aec9a72fa0
diegohpezet <diegohpezet@gmail.com>
2024-06-06 16:18:43 -0300

    chore: removed unnecessary console logs

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -2,14 +2,12 @@ const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
   const token = req.cookies.token;
-  console.log('JWT token:', token)
   if (token) {
     try {
       const decoded = jwt.verify(token, 'secret');
       req.user = decoded;
       res.locals.isLoggedIn = true; // Set a local variable for use in views
     } catch (error) {
-      console.log('Token is not valid');
       res.locals.isLoggedIn = false;
     }
   } else {
