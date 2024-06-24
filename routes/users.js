7c7da51df2a63c5369dc21bc3e99d94085dd1a70
diegohpezet <diegohpezet@gmail.com>
2024-06-24 16:55:54 -0300

    feat: add profile-related routes

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,9 @@ router.use(authenticateToken)
 
 router.get('/', UserController.getAll);
 router.get('/:id', UserController.getById);
+router.get('/:id/pictures', UserController.getPictures);
+router.get('/:id/pictures/liked', UserController.getLikedPictures);
+router.get('/:id/profile', UserController.renderProfile);
 router.put('/:id', UserController.update);
 router.put('/:id/password', UserController.updatePassword);
 router.delete('/:id', UserController.delete);
