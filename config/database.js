898fde038248889bc5030381824fe975e27f4620
diegohpezet <diegohpezet@gmail.com>
2024-07-15 13:26:27 -0300

    chore: swap to postgres

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 // Connect to DB
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mysql'
+  dialect: 'postgres'
 });
 
 // Test DB connection
